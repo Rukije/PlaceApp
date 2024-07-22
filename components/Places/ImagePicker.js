50687e11c4a158f3e753ff04797609eaf3ab3915
rukije <rukije.morina@harrisia.com>
2024-07-22 13:58:31 +0200

    Taking photos on Android and IOS + Managing Permissions

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,47 @@
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import { View, Button, Alert } from "react-native";
+
+function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permissions!",
+        "You need to grant camera permissios to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler}></Button>
+    </View>
+  );
+}
+
+export default ImagePicker;
